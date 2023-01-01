03181fdab3d09fe7ccceb46cea915456d1d9bad6
ohdy518 <ohdy518@gmail.com>
2023-01-01 22:50:12 +0900

    abcd

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var ohdytext = document.getElementById('ohdytext');
 ohdytext.addEventListener('click', opengithub);
+
+var clicktomail = document.getElementById('clicktomail');
+clicktomail.addEventListener('click', mailme);
+
+var clicktodiscord = document.getElementById('clicktodiscord');
+clicktodiscord.addEventListener('click', discordme);
+
 function opengithub()
 {
     open("https://github.com/ohdy518")
+}
+function mailme()
+{
+    open("mailto:ohdy518@gmail.com")
+}
+
+function discordme()
+{
+    alert("Sorry, my discord account is not public yet. Please email me instead. ")
 }
