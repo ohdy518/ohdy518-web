c23504b2ed19fbbda9b7efd3ee3a4a3b6067ddad
ohdy518 <ohdy518@gmail.com>
2023-01-02 11:04:20 +0900

    Add

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ clicktomail.addEventListener('click', mailme);
 var clicktodiscord = document.getElementById('clicktodiscord');
 clicktodiscord.addEventListener('click', discordme);
 
+var clicktoreddit = document.getElementById('clicktoreddit');
+clicktoreddit.addEventListener('click', redditme);
+
+var clicktohashnode = document.getElementById('clicktohashnode');
+clicktohashnode.addEventListener('click', hashnodeme);
+
+var clicktoyoutube = document.getElementById('clicktoyoutube');
+clicktoyoutube.addEventListener('click', youtubeme);
+
 function opengithub()
 {
     open("https://github.com/ohdy518")
@@ -19,4 +28,19 @@ function mailme()
 function discordme()
 {
     alert("Sorry, my discord account is not public yet. Please email me instead. ")
+}
+
+function redditme()
+{
+    open("https://reddit.com/u/ohdy518")
+}
+
+function hashnodeme()
+{
+    open("ohdy518.hashnode.dev")
+}
+
+function youtubeme()
+{
+    open("https://youtube.com/@ohdy518")
 }
